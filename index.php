ed60589ae797f33fafc452ce400a8ccb83ad45a0
Malikabhc <malika.bouhcine@gmail.com>
2018-07-26 13:36:33 +0200

    exercice5

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 5</title>
+  </head>
+  <body>
+    <?php
+      $int = NULL;
+      echo $int;
+      $int = 94;
+      echo $int;
+    ?>
+  </body>
+</html>
